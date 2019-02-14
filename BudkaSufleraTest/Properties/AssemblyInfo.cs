1e8a86fa8f92fcce39b13650ff00fa744367102c
Robert Ospara <robert.ospara@banner-mc.com>
2019-02-14 16:16:56 +0100

    initial commit

---
+++ BudkaSufleraTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BudkaSufleraTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BudkaSufleraTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bbd979c3-915b-4a9d-9bde-a0f0be216857")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BudkaSufleraTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BudkaSufleraTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string song = "JOLKA JOLKA PAMIETASZ LATO ZE SNU";
+            string crisWords = "JOLKA ZE SNU";
+
+            string output = checkSongContent(song, crisWords);
+
+            string [] allLines = output.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Assert(int.Parse(allLines[0]), 1);
+            foreach (var line in )
+            {
+
+            }
+        }
+    }
+}
